d6630a1c4356be70d12d3c23d259244039aae5e4
ko.terra.119@gmail.com <ko.terra.119@gmail.com>
2019-03-14 20:41:22 -0400

    GuessMyPick started

---
+++ GuessMyPick/GuessMyPick/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  GuessMyPick
+//
+//  Created by Mason Ko on 2019-03-14.
+//  Copyright © 2019 Mason Ko. All rights reserved.
+//
+
+import UIKit
+import Darwin // predefiend C functions
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
