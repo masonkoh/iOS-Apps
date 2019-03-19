1ceaa4c3a51e3f4257e003dd6949e88f529f3c78
ko.terra.119@gmail.com <ko.terra.119@gmail.com>
2019-03-19 03:32:18 -0400

    now it displays consoleLog on 'Label' on Main.Storyboard... I'm happy

--- gesture_swipe_tap_intro/gesture_swipe_tap_intro/ViewController.swift
+++ gesture_swipe_tap_intro/gesture_swipe_tap_intro/ViewController.swift
@@ -13,20 +13,28 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var myPinchLabel: UILabel!
     @IBOutlet weak var myTapLabel: UILabel!
+    @IBOutlet weak var myConsoleMsgLabel: UILabel!
     
     // function returns String(randomNo) for debugging purpose
     func randomNo() -> String {
         let randomNo : Int = Int(arc4random() & 100)
         return String(randomNo)
     }
+    func displayConsoleMsgOnLabel(consoleMsg: String) -> Void{
+        myConsoleMsgLabel.text = consoleMsg
+    }
+    
     
     @IBAction func mySwipeToRightHandler(_ sender: Any) {
+        displayConsoleMsgOnLabel(consoleMsg: "image swiped to right -  " + randomNo())
         print("image swiped to right -  " + randomNo())
     }
     @IBAction func mySwipeToLeftHandler(_ sender: Any) {
+        displayConsoleMsgOnLabel(consoleMsg: "image swiped to left -  " + randomNo())
         print("image swiped to left -  " + randomNo())
     }
     @IBAction func myTripleTappingImageHandler(_ sender: Any) {
+        displayConsoleMsgOnLabel(consoleMsg: "image tapped (triple touch!)- " + randomNo())
         print("image tapped (triple touch!)- " + randomNo())
     }
     
@@ -47,9 +55,11 @@ class ViewController: UIViewController {
     }
     
     @objc func myTapLabelTapped(recognizer: UITapGestureRecognizer){
+        displayConsoleMsgOnLabel(consoleMsg: "label(TAP ME!) double tapped - " + randomNo())
         print("label(TAP ME!) double tapped - " + randomNo())
     }
     @objc func myPinchLabelPinched(recognizer: UIPinchGestureRecognizer){
+        displayConsoleMsgOnLabel(consoleMsg: "label(PINCH ME!) pinched - " + randomNo())
         print("label(PINCH ME!) pinched - " + randomNo())
     }
     
