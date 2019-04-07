8271133e6eff70af1813906d76bb16dd27bafe64
ko.terra.119@gmail.com <ko.terra.119@gmail.com>
2019-04-07 10:31:26 -0400

    START: Standford iOS lecture -1

---
+++ Standford-developing iOS11 Apps with Swift - 1/Standford-developing iOS11 Apps with Swift - 1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Standford-developing iOS11 Apps with Swift - 1
+//
+//  Created by Mason Ko on 2019-04-07.
+//  Copyright © 2019 Mason Ko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
